04b3d3a534ad7eb1a88ee17cd9ec950194236923
NadiaA14 <nadia.almurshedy@hotmail.com>
2022-08-09 10:47:59 +0100

    first commit

---
+++ src/main/java/com/example/toptrumps/toptrumps/ToptrumpsApplication.java
@@ -0,0 +1,13 @@
+package com.example.toptrumps.toptrumps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToptrumpsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToptrumpsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/toptrumps/toptrumps/ToptrumpsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.toptrumps.toptrumps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToptrumpsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
